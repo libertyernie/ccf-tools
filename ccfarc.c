3e56d55e4c42040d3644a6db9f1817712ec417f7
libertyernie <maybeway36@gmail.com>
2021-03-13 21:05:18 -0600

    Add comments to ccfarc and ccfex

--- ccfarc.c
+++ ccfarc.c
@@ -4,14 +4,21 @@
 #include "zlib.h"
 
 const int ZERO = 0;
+
+// A template for a CCF header (as 32-bit numbers in little endian).
 const int HEADER[8] = {
 	0x00464343, 0x00000000, 0x00000000, 0x00000000,
 	0x00000020, 0x00000000, 0x00000000, 0x00000000};
 
+// A file descriptor for a CCF archive.
 typedef struct ccffile {
+	// The filename (null-padded ASCII)
 	char filename[20];
+	// The offset of the file's data with respect to the beginning of the CCF archive, in multiples of the chunk size in the archive's main header.
 	unsigned int offset;
+	// The number of bytes the file's data takes up within the archive.
 	unsigned int datasize;
+	// The number of bytes the file's data will take up once decompressed.
 	unsigned int filesize;
 } ccffile;
 
@@ -22,11 +29,13 @@ int main(int argc, char **argv) {
 	unsigned int files, size, curpos, i, j;
 	char *indata, *outdata;
 
+	// Ensure that there is an argument
 	if(argc < 2) {
 		fprintf(stderr, "USAGE: ccfex <infile1> [infile2] ...\n");
 		return(EXIT_FAILURE);
 	}
 
+	// Open output file stream
 	fprintf(stderr, "Beginning compression of out.ccf.\n");
 	outfile = fopen("out.ccf", "wb");
 	if(outfile == NULL) {
@@ -37,6 +46,8 @@ int main(int argc, char **argv) {
 	//prepare file entries as far as we can for now
 	files = argc - 1; //files to be inserted in to the archive
 	curpos = files + 1; //current position in 32 byte blocks, after header and file entries have been written (start of data region)
+
+	// Create a list of file descriptors
 	fileentries = (ccffile **)malloc(sizeof(ccffile *) * files);
 	if(fileentries == NULL) {
 		fprintf(stderr, "Couldn't allocate enough memory!\n");
@@ -44,17 +55,21 @@ int main(int argc, char **argv) {
 	}
 	for(i = 0; i < files; i++) {
 		fprintf(stderr, "%u %s ", i, argv[i + 1]);
+		// Create a new file descriptor
 		fileentries[i] = (ccffile *)malloc(sizeof(ccffile));
 		if(fileentries[i] == NULL) {
 			fprintf(stderr, "Couldn't allocate enough memory!\n");
 			return(EXIT_FAILURE);
 		}
+		// Truncate the filename to twenty characters if necessary
 		int t = strlen(argv[i + 1]);
 		if(t > 20) {
 			t = 20;
 		}
 		memcpy(fileentries[i]->filename, argv[i + 1], t);
+		// Open the input file stream
 		infile = fopen(argv[i + 1], "rb");
+		// Determine the uncompressed filesize
 		fseek(infile, 0, SEEK_END);
 		size = ftell(infile);
 		fileentries[i]->filesize = size;
@@ -77,26 +92,32 @@ int main(int argc, char **argv) {
 	//compress and store files, then update the file entries
 	for(i = 0; i < files; i++) {
 		fprintf(stderr, ".");
+		// Copy the input data to memory
 		indata = (char *)malloc(sizeof(char) * fileentries[i]->filesize);
 		if(indata == NULL) {
 			fprintf(stderr, "Couldn't allocate enough memory!\n");
 			return(EXIT_FAILURE);
 		}
+		// Create a place for the output data to go
 		outdata = (char *)malloc(sizeof(char) * fileentries[i]->datasize);
 		if(outdata == NULL) {
 			fprintf(stderr, "Couldn't allocate enough memory!\n");
 			return(EXIT_FAILURE);
 		}
 
+		// Open the input file stream again
 		infile = fopen(argv[i + 1], "rb");
 		if(infile == NULL) {
 			fprintf(stderr, "Couldn't open file %s\n", argv[i + 1]);
 			return(EXIT_FAILURE);
 		}
+		// Copy to memory
 		fread(indata, 1, fileentries[i]->filesize, infile);
 		fclose(infile);
 
+		// Retrieve the buffer size
 		long datasize = (long)fileentries[i]->datasize;
+		// Perform compression, telling zlib to put the correct compressed size in the correct place in the header
 		retval = compress(outdata, &datasize, indata, fileentries[i]->filesize);
 		switch(retval) {
 			case Z_OK:
@@ -112,17 +133,25 @@ int main(int argc, char **argv) {
 				return(EXIT_FAILURE);
 		}
 
+		// Ensure that the compressed size is 32 bits
 		fileentries[i]->datasize = datasize & 0xFFFFFFFF;
+		// Check whether the file became smaller
 		if(fileentries[i]->datasize > fileentries[i]->filesize) {
+			// The file is smaller now that it has been compressed - write the compressed data
 			fileentries[i]->datasize = fileentries[i]->filesize;
 			fwrite(indata, 1, fileentries[i]->filesize, outfile);
 		} else {
+			// Just write the original uncompressed data
 			fwrite(outdata, 1, fileentries[i]->datasize, outfile);
 		}
+		// Add the file data's correct position to its file descriptor, and advance the next file's position as appropriate
 		fileentries[i]->offset = curpos;
 		curpos += fileentries[i]->datasize / 32 + 1;
+		// Jump to the position in the output file stream where the file descriptor should go
 		fseek(outfile, 32 * i + 32, SEEK_SET);
+		// Write the file descriptor
 		fwrite(fileentries[i], sizeof(ccffile), 1, outfile);
+		// Jump to the end of the file so we can pad it to a multiple of the block size of 32 bytes
 		fseek(outfile, 0, SEEK_END);
 		int end = ftell(outfile);
 		for(j = 0; j < 32 - (end % 32); j++) { //pad out to next block
--- ccfex.c
+++ ccfex.c
@@ -6,19 +6,26 @@
 #define MAGIC (0x00464343)
 #define ZLIB_CHUNK
 
+// A file descriptor for a CCF archive.
 typedef struct ccffile {
+	// The filename (null-padded ASCII)
 	char filename[20];
+	// The offset of the file's data with respect to the beginning of the CCF archive, in multiples of the chunk size in the archive's main header.
 	unsigned int offset;
+	// The number of bytes the file's data takes up within the archive.
 	unsigned int datasize;
+	// The number of bytes the file's data will take up once decompressed.
 	unsigned int filesize;
 } ccffile;
 
 int main(int argc, char **argv) {
+	// Ensure that there is an argument
 	if(argc < 2) {
 		fprintf(stderr, "USAGE: ccfex <ccffile>\n");
 		return(EXIT_FAILURE);
 	}
 
+	// Open the file stream
 	fprintf(stderr, "Beginning extraction of %s.\n", argv[1]);
 	FILE *infile = fopen(argv[1], "rb");
 	if(infile == NULL) {
@@ -26,33 +33,42 @@ int main(int argc, char **argv) {
 		return(EXIT_FAILURE);
 	}
 
+	// Local variables
 	unsigned int magic, chunksize, filecount, i;
+	// A list of file descriptors
 	ccffile **fileentries;
+	// A buffer on the stack for reading filenames from the file descriptor that will always have a null terminator after 20 characters
 	char temp[21] = "                    ";
+	// Verify the header (C, C, F, 0x00)
 	fread(&magic, 4, 1, infile);
 	if(magic != MAGIC) {
 		fprintf(stderr, "File %s isn't a valid CCF archive. %X\n", argv[1], magic);
 		return(EXIT_FAILURE);
 	}
+	// Jump to 0x10 and read the chunk size and number of files
 	fseek(infile, 16, SEEK_SET); fread(&chunksize, 4, 1, infile);
 	fseek(infile, 20, SEEK_SET); fread(&filecount, 4, 1, infile);
 	if(filecount == 0) {
 		fprintf(stderr, "File %s contains no files.\n", argv[1]);
 		return(EXIT_FAILURE);
 	}
+	// Allocate a list to store pointers to file descriptors
 	fileentries = (ccffile **)malloc(sizeof(ccffile *) * filecount);
 	if(fileentries == NULL) {
 		fprintf(stderr, "Couldn't allocate memory for file list.\n");
 		return(EXIT_FAILURE);
 	}
+	// Jump to end of header
 	fseek(infile, 0x20, SEEK_SET);
 	for(i = 0; i < filecount; i++) {
+		// Copy the file descriptor into the "fileentries" list
 		fileentries[i] = (ccffile *)malloc(sizeof(ccffile) * filecount);
 		if(fileentries[i] == NULL) {
 			fprintf(stderr, "Couldn't allocate memory for file list.\n");
 			return(EXIT_FAILURE);
 		}
 		fread(fileentries[i], 32, 1, infile);
+		// Copy the name into the temporary buffer
 		memcpy(temp, fileentries[i]->filename, 20);
 		fprintf(stderr, "%u  %s  %u %u %u\n", i, temp, fileentries[i]->offset, fileentries[i]->datasize, fileentries[i]->filesize);
 	}
@@ -61,18 +77,24 @@ int main(int argc, char **argv) {
 	int retval;
 	FILE *outfile;
 	for(i = 0; i < filecount; i++) {
+		// Copy the name into the temporary buffer
 		memcpy(temp, fileentries[i]->filename, 20);
+		// Open the output file stream
 		outfile = fopen(temp, "wb");
 		if(outfile == NULL) {
 			fprintf(stderr, "Couldn't open %s for writing.\n", temp);
 			return(EXIT_FAILURE);
 		}
+		// Jump to the beginning of the data for this file in the input stream
 		fseek(infile, fileentries[i]->offset * chunksize, SEEK_SET);
+		// Copy compressed data to a memory buffer capable of holding it
 		databuffer = (char *)malloc(sizeof(char) * fileentries[i]->datasize);
 		fread(databuffer, 1, fileentries[i]->datasize, infile);
 		if(fileentries[i]->filesize == fileentries[i]->datasize) {
+			// Uncompressed data - copy to output stream
 			fwrite(databuffer, 1, fileentries[i]->datasize, outfile);
 		} else {
+			// Uncompress the data
 			filebuffer = (char *)malloc(sizeof(char) * fileentries[i]->filesize);
 			long size = fileentries[i]->filesize;
 			retval = uncompress(filebuffer, &size, databuffer, fileentries[i]->datasize);
@@ -89,6 +111,7 @@ int main(int argc, char **argv) {
 					fprintf(stderr, "The compressed data is corrupted.\n");
 					return(EXIT_FAILURE);
 			}
+			// Write the uncompressed data to the output stream
 			fwrite(filebuffer, 1, size, outfile);
 			free(filebuffer);
 		}
